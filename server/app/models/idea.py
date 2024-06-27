10677e7a4a8513aaa46efbf9cb5bed3a692d9b92
VuLemon <vule@brandeis.edu>
2024-06-27 14:54:25 -0400

    Added DateTime and Upvote sorting

--- server/app/models/idea.py
+++ server/app/models/idea.py
@@ -1,11 +1,13 @@
 import mongoengine as db
 import uuid
+import datetime
 
 class Idea(db.EmbeddedDocument):
     id = db.UUIDField(required = True, default=uuid.uuid4)
     title = db.StringField(required=True)
     description = db.StringField(required=True)
     votes = db.IntField(default=0)
+    date_added = db.DateTimeField(default=datetime.datetime.now)
 
     def to_json(self):
         return {
@@ -13,4 +15,5 @@ class Idea(db.EmbeddedDocument):
             "title": self.title,
             "description": self.description,
             "votes": self.votes,
+            "date_added": self.date_added.isoformat(),
         }
